76c3a1266fc1b65377757be13148a32010a34e74
ibas2020 <ibas2020fiz@gmail.com>
2020-09-08 22:29:05 +0300

    Обновления на 08.09

---
+++ Practice_07.09/HomeWork/cmake-build-debug/CMakeFiles/HomeWork.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork.dir/main.cpp.o"
+  "HomeWork"
+  "HomeWork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
