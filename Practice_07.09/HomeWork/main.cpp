fce7829a9357eb83427ed6850d7b44d3b729f001
ibas2020 <ibas2020fiz@gmail.com>
2020-09-09 21:16:44 +0300

    Д/з за 07.09

--- Practice_07.09/HomeWork/main.cpp
+++ Practice_07.09/HomeWork/main.cpp
@@ -7,6 +7,24 @@
 using namespace std;
 
 int main() {
-    
+    int a, b, c;
+    cout << "Введите числа a, b и с: ";
+    cin >> a >> b >> c;
+    if (a > b && a > c)
+        if (b > c)
+            cout << "max = " << a << ", min = " << c;
+        else
+            cout << "max = " << a << ", min = " << b;
+    else
+        if (b > c && b > a)
+            if (a > c)
+                cout << "max = " << b << ", min = " << c;
+            else
+                cout << "max = " << b << ", min =" << a;
+        else
+            if (a > b)
+                cout << "max = " << c << ", min = " << b;
+            else
+                cout << "max = " << c << ", min = " << a;
     return 0;
 }
